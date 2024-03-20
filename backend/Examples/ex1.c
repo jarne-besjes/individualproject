72b3f4e16514f0822c4b86597f83a2355d3c355f
glopjevdb <91670760+glopjevdb@users.noreply.github.com>
2024-03-20 12:16:58 +0100

    Add examples

---
+++ backend/Examples/ex1.c
@@ -0,0 +1,14 @@
+
+
+int main() {
+    int x = 1;
+    int y = 1;
+    int z = 1;
+    while (*) {
+        x = 2*x;
+        y = 4*y;
+        z = 8*z;
+    }
+
+    return 0;
+}
---
+++ backend/Examples/ex2.c
@@ -0,0 +1,12 @@
+
+
+int main() {
+    int x = 1;
+    int y = 1;
+    while (*) {
+        x = 2*x;
+        y = 2*y;
+    }
+
+    return 0;
+}
