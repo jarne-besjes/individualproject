c71fb8c067152aa7fda79e3956b7163091d2f657
jarne-besjes <91670760+jarne-besjes@users.noreply.github.com>
2024-06-20 16:20:17 +0200

    Add logo

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,12 +1,19 @@
 import CodeEditor from "../../components/CodeEditor";
 import './code-editor.css';
+import './home.css';
+import logo from '../../logo.svg';
 
 export default function Home() {
     return (
+        <div id="home">
+        <div id="logo">
+            <img src={logo} className="CompLogo" alt="logo" />
+        </div>
         <div id="code-editor-div">
             <h1>Code Editor:</h1>
             <CodeEditor></CodeEditor>
         </div>
+    </div>
 )
 
 }
